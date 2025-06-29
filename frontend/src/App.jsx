2924eab30a02205c9232931cc0397bca228c95c0
Ani <intrepidani05@gmail.com>
2025-06-30 00:55:56 +0530

    feat: integrate react-hot-toast for toast notifications; wrap RouterProvider with ThemeProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,40 @@
 import { RouterProvider } from "react-router";
 import router from "./router.jsx";
+import { ThemeProvider } from "./context/themeContext.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <ThemeProvider>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 3000,
+          removeDelay: 100,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+        }}
+      />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
