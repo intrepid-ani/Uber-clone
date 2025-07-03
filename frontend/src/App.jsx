10e5e510b043806520762822c1a774558de20cbf
Ani <intrepidani05@gmail.com>
2025-07-03 20:25:58 +0530

    feat: replace ThemeProvider with Captain and User context providers in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,44 @@
 import { RouterProvider } from "react-router";
 import router from "./router.jsx";
-import { ThemeProvider } from "./context/themeContext.jsx";
 import { Toaster } from "react-hot-toast";
+import { CaptainDataProvider } from "./context/CaptainContext.jsx";
+import { UserDataProvider } from "./context/UserContext.jsx";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Toaster
-        position="bottom-right"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Define default options
-          className: "",
-          duration: 3000,
-          removeDelay: 100,
-          style: {
-            background: "#363636",
-            color: "#fff",
-          },
+    <>
+      <CaptainDataProvider>
+        <UserDataProvider>
+          <Toaster
+            position="bottom-right"
+            reverseOrder={false}
+            gutter={8}
+            containerClassName=""
+            containerStyle={{}}
+            toastOptions={{
+              // Define default options
+              className: "",
+              duration: 3000,
+              removeDelay: 100,
+              style: {
+                background: "rgb(23 23 23)",
+                color: "#fff",
+              },
 
-          // Default options for specific types
-          success: {
-            duration: 3000,
-            iconTheme: {
-              primary: "green",
-              secondary: "black",
-            },
-          },
-        }}
-      />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+              // Default options for specific types
+              success: {
+                duration: 3000,
+                iconTheme: {
+                  primary: "green",
+                  secondary: "black",
+                },
+              },
+            }}
+          />
+          <RouterProvider router={router} />
+        </UserDataProvider>
+      </CaptainDataProvider>
+    </>
   );
 }
 
